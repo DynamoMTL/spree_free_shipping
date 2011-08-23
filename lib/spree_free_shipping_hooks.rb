e2790dbdf5ed6f92a1f8f3d1eb859050ba0ddbf0
Bryan Mahoney <bryan@goDynamo.com>
2011-08-23 16:24:25 -0400

    updates hooks document so that the settings link appears properly

--- lib/spree_free_shipping_hooks.rb
+++ lib/spree_free_shipping_hooks.rb
@@ -1,7 +1,10 @@
 class SpreeFreeShippingHooks < Spree::ThemeSupport::HookListener
 
   insert_after :admin_configurations_menu do
-    %(<%= tab :free_shipping_settings %>)
+    '<tr>
+      <td><%= link_to t("free_shipping_settings"), admin_free_shipping_settings_path %></td>
+      <td><%= t("free_shipping_settings_description") %></td>
+    </tr>'
   end
   
 end
